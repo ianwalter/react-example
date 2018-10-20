4a0d85b41e198d1fea29d3e3b3906ffeb8fcafa3
Ian Walter <public@iankwalter.com>
2018-10-19 20:27:15 -0400

    Updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
-const path = require('path')
-const webpack = require('webpack')
+const { join } = require('path')
+
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: './src/main.jsx',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'js/[name].bundle.js',
     chunkFilename: 'js/[id].[chunkhash].js'
@@ -16,17 +14,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
     alias: {
-      '@': path.join(__dirname, 'src')
+      '@': join(__dirname, 'src')
     }
   },
   plugins: [
-    ...(isProduction ? [new webpack.optimize.UglifyJsPlugin({})] : []),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'common' }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({ title: 'Pagesurf', template: 'src/index.html' })
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
   ],
   module: {
     rules: [
